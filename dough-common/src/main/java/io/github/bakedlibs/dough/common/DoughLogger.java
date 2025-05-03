f751567196fdd00a46b178580398a8ed5ab5a726
mcchampions <1309635304@qq.com>
2025-05-03 17:18:29 +0800

    Update DoughLogger.java

--- dough-common/src/main/java/io/github/bakedlibs/dough/common/DoughLogger.java
+++ dough-common/src/main/java/io/github/bakedlibs/dough/common/DoughLogger.java
@@ -5,7 +5,6 @@ import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Server;
-import org.bukkit.plugin.PluginLogger;
 
 public class DoughLogger extends Logger {
     public DoughLogger(Server server, String name) {
